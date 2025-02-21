5c8484f52f8ecefc0c5ff244b7c6d859f1cc94cb
Siyabonga Magwaza <ssmagwaza88@gmail.com>
2025-02-21 19:55:36 +0200

    added Integration test

--- test/unit/DeployMoodNftTest.t.sol
+++ test/unit/DeployMoodNftTest.t.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.24;
 
 import {Test, console} from "forge-std/Test.sol";
-import {DeployMoodNft} from "../script/DeployMoodNft.s.sol";
-import {MoodNft} from "../src/MoodNft.sol";
+import {DeployMoodNft} from "../../script/DeployMoodNft.s.sol";
+import {MoodNft} from "../../src/MoodNft.sol";
 
 
 contract DeployMoodNftTest is Test {
